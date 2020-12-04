66a02a751b6ef80a00f547d6f0bbdac84630b425
AhmadRazaJamal <ahmad.raza@me.com>
2020-12-03 22:49:46 -0600

    Made a couple changes to add sagas for async actions

--- src/reducers/story.js
+++ src/reducers/story.js
@@ -1,24 +1,16 @@
-const INITIAL_STATE = [
-    {
-        title: 'React',
-        url: 'https://facebook.github.io/react/',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-    }, {
-        title: 'Redux',
-        url: 'https://github.com/reactjs/redux',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-    },
-];
+import { STORIES_ADD } from '../constants/actionTypes';
 
-function storyReducer(state = INITIAL_STATE, action){
-    switch (action.type){
-        default: return state; 
+const INITIAL_STATE = [];
+
+const applyAddStories = (state, action) =>
+    action.stories;
+
+function storyReducer(state = INITIAL_STATE, action) {
+    switch (action.type) {
+        case STORIES_ADD: {
+            return applyAddStories(state, action);
+        }
+        default: return state;
     }
 }
 
